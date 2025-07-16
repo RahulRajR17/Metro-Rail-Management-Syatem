e9ee8bbdab6e7f930539b47be6d0bd597632943e
RahulRajR17 <rahulrajr2002@gmail.com>
2025-07-16 19:44:47 +0530

    Metro

---
+++ home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+    <head>
+        <title>Home Page</title>
+    </head>
+    <body style="background:linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(wallpaper.jpg); background-repeat:no-repeat; background-size:cover;  ">
+    
+    <p style="font-size:60px" align="center"><strong><color='white'>BENGALURU METRO RAIL CORPORATION LIMITED (BMRCL)</strong></p> 
+    
+        <br>
+    <br>
+    
+    
+    
+    <p> <font size="5px" color="white" face="Courier New"> Bengaluru Metro Rail Corporation Limited (BMRCL), a joint venture of Government of India and the State Government of Karnataka, is the agency for building, operting and expanding the Namma Metro network. Namma Metro or Bengaluru Metro, is a rapid transit system serving the city of Bengaluru. Upon its inauguration, it became the first Metro system in South India. The metro has a mix of underground, at grade, and elivated stations. The system runs on standard gauge tracks.
+   </font> </p>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,160 @@
+﻿<!DOCTYPE html>
+
+
+<head>
+	
+<meta charset="utf-8">
+	
+<meta name="viewport" content="width=devidev-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Metro train Reservation</title>
+	<body style="background:linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(logo.jpg); background-repeat:no-repeat; background-size:cover;  ">
+	
+<!-- [ FONT-AWESOME ICON ] 
+        
+=========================================================================================================================-->
+	
+<link rel="stylesheet" type="text/css" href="library/font-awesome-4.3.0/css/font-awesome.min.css">
+
+	
+<!-- [ PLUGIN STYLESHEET ]
+        
+=========================================================================================================================-->
+	
+<link rel="shortcut icon" type="image/x-icon" href="images/icon.png">
+	
+<link rel="stylesheet" type="text/css" href="css/animate.css">
+	
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+        
+<link rel="stylesheet" type="text/css" href="css/owl.theme.css">
+	
+<link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+	
+<!-- [ Boot STYLESHEET ]
+        
+=========================================================================================================================-->
+	
+<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap-theme.min.css">
+	
+<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap.css">
+       
+<!-- [ DEFAULT STYLESHEET ] 
+        
+=========================================================================================================================-->
+	
+<link rel="stylesheet" type="text/css" href="css/style.css">
+        
+<link rel="stylesheet" type="text/css" href="css/responsive.css">
+	
+<link rel="stylesheet" type="text/css" href="css/color/rose.css">
+        
+
+</head>
+<body>
+
+
+<!-- Navigation
+    ==========================================-->
+    
+<nav  class=" nim-menu navbar navbar-default navbar-fixed-top" >
+      
+<div class="container" >
+        
+
+<!-- Collect the nav links, forms, and other content for toggling -->
+        
+<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          
+<ul class="nav navbar-nav navbar-right">
+            
+<li><a href="home.php" class="page-scroll"><h4>Home</h4></a></li>
+
+<li><a href="admin/" class="page-scroll"><h4>Login</h4></a></li>
+          
+</ul>
+        
+</div>
+<!-- /.navbar-collapse -->
+      
+</div><!-- /.container-fluid -->
+    
+</nav>
+
+   
+<!-- [/MAIN-HEADING]
+ 
+============================================================================================================================--> 
+   
+<section style="text-align: center;" id="home" >
+          
+<div class="overlay">
+           
+<div class="container"  >
+    <br>
+    <br>
+    <br>
+    <br>
+    <p sytle="font-family: Fantasy"><h1>WELCOME TO METRO TRAIN RESERVATION SYSTEM</h1></p> 
+    <br>
+<br>
+<p sytle="font-family: Fantasy"><h1>Online Ticket Booking</h1></p>         
+<div class="row" style="padding: 70px;">
+<br>
+<a href="reserved.php" class="btn btn-default" style="font-family: Papyrus;">Book Now</a>
+                 
+</div>
+      
+</div>
+               
+</div>
+           
+</div>
+       
+</div>  
+   
+</section>
+ 
+
+<!-- [ DEFAULT SCRIPT ] -->
+	
+<script src="library/modernizr.custom.97074.js"></script>
+	
+<script src="library/jquery-1.11.3.min.js"></script>
+        
+<script src="library/bootstrap/js/bootstrap.js"></script>
+	
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>	
+	
+<!-- [ PLUGIN SCRIPT ] -->
+        
+<script src="library/vegas/vegas.min.js"></script>
+	
+<script src="js/plugins.js"></script>
+        
+<!-- [ TYPING SCRIPT ] -->
+         
+<script src="js/typed.js"></script>
+         
+<!-- [ COUNT SCRIPT ] -->
+         
+<script src="js/fappear.js"></script>
+       
+<script src="js/jquery.countTo.js"></script>
+	
+<!-- [ SLIDER SCRIPT ] -->  
+        
+<script src="js/owl.carousel.js"></script>
+         
+<script src="js/jquery.magnific-popup.min.js" type="text/javascript"></script>
+        
+<script type="text/javascript" src="js/SmoothScroll.js"></script>
+        
+        
+<!-- [ COMMON SCRIPT ] -->
+	<script src="js/common.js"></script>
+  
+</body>
+
+</html>
